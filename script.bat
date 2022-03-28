5d9000b00c81f5bb0dfdf1a07eb5159cf3133f8e
TarcisioLimaAmaral <tarcisiolavw@gmail.com>
2022-03-28 19:46:52 -0300

    Testando

---
+++ script.bat
@@ -0,0 +1,9 @@
+cd c:\exemplo
+echo "Criando Pasta ! Aguarde"
+timeout /t 5
+mkdir outros
+echo "Teste de arquivo - Serratec 2022" > arquivo6.txt
+mkdir outros-exemplos
+echo "Teste de aquivo - Serratec 2022" > arquivo.txt
+timeout /t 10
+echo "Fim de script!!!"
